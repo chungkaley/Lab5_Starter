170db083289107b0e91883d9b5df8b5ec48353c9
chungkaley <chungkaley@gmail.com>
2026-05-04 20:39:53 -0700

    testing branch: should be wrong

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,9 +2,9 @@
 import { sum as sum2 } from '../code-to-unit-test/sum';
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+    expect(1 + 2).toBe(4);
 });
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum2(1, 2)).toBe(3);
+    expect(sum2(1, 2)).toBe(4);
 });
